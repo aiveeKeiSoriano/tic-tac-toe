8c98ab007fc9e951aa2f8a85bbbef01729e906b1
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-04-28 22:28:32 +0800

    fixed winning scenario

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,10 +5,28 @@ import History from './History'
 export default function Game(props) {
     let [turn, setTurn] = useState('X')
     let [val, setVal] = useState(new Array(9).fill(' '))
-    let [status, setStatus] = useState('Player X\'s turn')
+    let [status, setStatus] = useState(['Player X\'s turn', false])
 
     let changeVal = (index, value) => {
-        if (value === ' ') {   
+        if (status[1]) {
+            if (!checkBoard(val, turn)) {
+                let prevTurn = turn
+                let copy = val.slice()
+                copy[index] = turn;
+                if (turn === 'X') {
+                    setTurn('O')
+                }
+                else {
+                    setTurn('X')
+                }
+                storeState(copy)
+                setVal(copy)
+                if (prevTurn === 'X') prevTurn = 'O'
+                else prevTurn = 'X'
+                checkBoard(copy, prevTurn)
+            }
+        }
+        if (value === ' ' && !status[1]) {
             let prevTurn = turn
             let copy = val.slice()
             copy[index] = turn;
@@ -27,16 +45,46 @@ export default function Game(props) {
     }
 
     let checkBoard = (arr, player) => {
-        if (arr[0] + arr[1] + arr[2] === 'XXX' || arr[0] + arr[1] + arr[2] === 'OOO') setStatus('Player ' + arr[0] + ' won!')
-        else if (arr[3] + arr[4] + arr[5] === 'XXX' || arr[3] + arr[4] + arr[5] === 'OOO') setStatus('Player ' + arr[3] + ' won!')
-        else if (arr[6] + arr[7] + arr[8] === 'XXX' || arr[6] + arr[7] + arr[8] === 'OOO') setStatus('Player ' + arr[6] + ' won!')
-        else if (arr[0] + arr[3] + arr[6] === 'XXX' || arr[0] + arr[3] + arr[6] === 'OOO') setStatus('Player ' + arr[0] + ' won!')
-        else if (arr[1] + arr[4] + arr[7] === 'XXX' || arr[1] + arr[4] + arr[7] === 'OOO') setStatus('Player ' + arr[1] + ' won!')
-        else if (arr[2] + arr[5] + arr[8] === 'XXX' || arr[2] + arr[5] + arr[8] === 'OOO') setStatus('Player ' + arr[2] + ' won!')
-        else if (arr[0] + arr[4] + arr[8] === 'XXX' || arr[0] + arr[4] + arr[8] === 'OOO') setStatus('Player ' + arr[0] + ' won!')
-        else if (arr[2] + arr[4] + arr[6] === 'XXX' || arr[2] + arr[4] + arr[6] === 'OOO') setStatus('Player ' + arr[2] + ' won!')
-        else if (arr.every(el => el !== ' ')) setStatus('Draw :)')
-        else setStatus('Player ' + player + '\'s turn')
+        if (arr[0] + arr[1] + arr[2] === 'XXX' || arr[0] + arr[1] + arr[2] === 'OOO') {
+            setStatus(['Player ' + arr[0] + ' won!', true])
+            return true
+        }
+        else if (arr[3] + arr[4] + arr[5] === 'XXX' || arr[3] + arr[4] + arr[5] === 'OOO') {
+            setStatus(['Player ' + arr[3] + ' won!', true])
+            return true
+        }
+        else if (arr[6] + arr[7] + arr[8] === 'XXX' || arr[6] + arr[7] + arr[8] === 'OOO') {
+            setStatus(['Player ' + arr[6] + ' won!', true])
+            return true
+        }
+        else if (arr[0] + arr[3] + arr[6] === 'XXX' || arr[0] + arr[3] + arr[6] === 'OOO') {
+            setStatus(['Player ' + arr[0] + ' won!', true])
+            return true
+        }
+        else if (arr[1] + arr[4] + arr[7] === 'XXX' || arr[1] + arr[4] + arr[7] === 'OOO') {
+            setStatus(['Player ' + arr[1] + ' won!', true])
+            return true
+        }
+        else if (arr[2] + arr[5] + arr[8] === 'XXX' || arr[2] + arr[5] + arr[8] === 'OOO') {
+            setStatus(['Player ' + arr[2] + ' won!', true])
+            return true
+        }
+        else if (arr[0] + arr[4] + arr[8] === 'XXX' || arr[0] + arr[4] + arr[8] === 'OOO') {
+            setStatus(['Player ' + arr[0] + ' won!', true])
+            return true
+        }
+        else if (arr[2] + arr[4] + arr[6] === 'XXX' || arr[2] + arr[4] + arr[6] === 'OOO') {
+            setStatus(['Player ' + arr[2] + ' won!', true])
+            return true
+        }
+        else if (arr.every(el => el !== ' ')) {
+            setStatus(['Draw :)', true])
+            return true
+        }
+        else {
+            setStatus(['Player ' + player + '\'s turn', false])
+            return false
+        }
     }
 
     let [states, setStates] = useState([val])
@@ -60,7 +108,7 @@ export default function Game(props) {
 
     return (
         <div className="container">
-            <Board values={val} stats={status} click={changeVal} />
+            <Board values={val} stats={status[0]} click={changeVal} />
             <div className="history">
                 <h1>Game History</h1>
                 <div className="historyBoard">
